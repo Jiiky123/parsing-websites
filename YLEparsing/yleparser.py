ed59b44216199d7fa419bb5b24cea730e45c32db
Joakim Fagerholm <joakim.fagerholm@gmail.com>
2019-04-23 16:18:09 +0300

    converted author from list to str. save to xlsx

--- YLEparsing/yleparser.py
+++ YLEparsing/yleparser.py
@@ -1,60 +1,69 @@
 from bs4 import BeautifulSoup
 import requests
+from urllib.request import urlopen
 import re
 import pandas as pd
+import json
+import os
+os.chdir('D:/PythonProjektATOM/Git/Repositories/parsing-with-bs4/YLEparsing')
 
-# get source code & soup it
-source = requests.get('https://yle.fi/uutiset/18-204933').text
-soup = BeautifulSoup(source, 'lxml')
+# grab url and get source
+url = 'https://yle-fi-search.api.yle.fi/v1/search?app_id=hakuylefi_v2_prod&app_key=4c1422b466ee676e03c4ba9866c0921f&language=fi&limit=2000&offset=0&query=talous'
+with urlopen(url) as response:
+    source = response.read()
 
-# get article
-article = soup.find('div', class_='yle__article__listItem__textualContent')
+data = json.loads(source.decode('utf-8'))
+# print(json.dumps(data, indent=2))
 
 
-def parse_yle_articles():
-    # create dataframe for content
-    yle_articles = pd.DataFrame(columns=['date', 'title', 'lead', 'category'])
+def article_df():
 
-    # lists for article pieces
+    # list of items in articles
     dates = []
-    titles = []
+    headlines = []
     leads = []
-    categories = []
+    authors = []
 
-    for article in soup.find_all('article'):
+    for item in data['data']:  # fill lists
+        try:
+            date = item['datePublished'].replace('\u2009', '')
+            headline = item['headline'].replace('\u2009', '')
+            lead = item['lead'].replace('\u2009', '')
+            author = ''.join(item['author'])
+        except KeyError:
+            print('data not found')
+        finally:  # continue loop even if not found
+            pass
 
-        # get date
-        date = soup.find('time', class_='yle__article__listItem__meta__published')['datetime']
-        date = pd.Series(date.split('T')[0])
-
-        # get title
-        title = article.h1.text
-
-        # get lead of article
-        lead = article.p.text
-
-        # get category
-        category = soup.find(
-            'span', class_=re.compile(r'yle__subject yle__borderColor')).text
-
-        # append lists
+        # append to lists
         dates.append(date)
-        titles.append(title)
+        headlines.append(headline)
         leads.append(lead)
-        categories.append(category)
+        authors.append(author)
 
-    # append everything to pd dataframe
-    yle_articles['date'] = dates
-    yle_articles['title'] = titles
-    yle_articles['lead'] = leads
-    yle_articles['category'] = categories
+    # move everything to pandas dataframe
+    talous_articles = pd.DataFrame(
+        columns=['date', 'headline', 'lead', 'author'])
+    talous_articles.date = dates
+    talous_articles.headline = headlines
+    talous_articles.lead = leads
+    talous_articles.author = authors
 
-    return yle_articles
+    # clean up data
+    talous_articles = talous_articles.drop_duplicates(subset='headline')
+    talous_articles.date = pd.to_datetime(
+        talous_articles.date, format='%Y-%m-%d', errors='coerce').dt.date
+    talous_articles.sort_values('date', inplace=True, ascending=False)
+    talous_articles.set_index('date', inplace=True)
+
+    # check dataframe
+    print(talous_articles.info())
+    print(talous_articles.isna().sum())
+    print(talous_articles.lead.head())
+
+    # finally save to excel
+    talous_articles.to_excel('yle_articles.xlsx')
+    print('saved as xlsx file')
 
 
-def split_words(object):  # figure out how to split series of strings
-    pass
-
-
-def most_used_words(splitwords):  # count most used words (keywords)
-    pass
+article_df()
