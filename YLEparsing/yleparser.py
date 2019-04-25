064da130ce4d9fccec3ac3a3af20cce9ef461287
Joakim Fagerholm <joakim.fagerholm@gmail.com>
2019-04-25 15:13:31 +0300

    clean code

--- YLEparsing/yleparser.py
+++ YLEparsing/yleparser.py
@@ -10,38 +10,41 @@ import matplotlib.pyplot as plt
 
 # CLASS FUNCTIONS ------------------------------------------------------------
 
-# __init__(searchword, search, limit=20000, offset=0, language='sv')
-# search is the category you are looking for
+# __init__(query, search, limit=20000, offset=0, language='sv')
+# category is the category you are looking for in articles
 # limit is number of articles
 # offset if you want to start from a given nr of articles
 # languages are swedish=sv, finnish=fi, english=en
 
-# save_excel(searchword, filename, loc)
+# save_excel(query, filename, loc)
 # example: articles.save_excel('sports', '../data/')
 
-# most_common_words(searchword, word=None)
+# most_common_words(query, word=None)
 # gives a list of 150 most common words in category
 # word=None can be changed if you want to know count of specific word
 
-# word_over_time(searchword, *words, c='b')
+# word_over_time(query, *words, c='b')
 # takes unlimited words as argument
 # plots all words and their popularity over time
 
 
-# GET ARTICLES AND CREATE DATAFRAME-------------------------------------------
+# GET ARTICLES AND CREATE DATAFRAME---------------------------------------------
 
 
 class GetArticles:
 
-    def __init__(searchword, search, limit=20000, offset=0, language='sv'):
-        searchword.search = search
-        searchword.limit = limit
-        searchword.offset = offset
-        searchword.language = language
+    def __init__(query, category, limit=20000, offset=0, language='sv'):
+        query.category = category
+        query.limit = limit
+        query.offset = offset
+        query.language = language
 
         # grab url and get source (notice in URL: limit, offset, query)
-        url = 'https://yle-fi-search.api.yle.fi/v1/search?app_id=hakuylefi_v2_prod&app_key=4c1422b466ee676e03c4ba9866c0921f&language={}&limit={}&offset={}&query={}'.format(
-            searchword.language, searchword.limit, searchword.offset, searchword.search)
+        url = 'https://yle-fi-search.api.yle.fi/v1/search?app_id=\
+        hakuylefi_v2_prod&app_key=4c1422b466ee676e03c4ba9866c0921f&\
+        language={}&limit={}&offset={}&query={}'.format(
+            query.language, query.limit,
+            query.offset, query.category)
 
         with urlopen(url) as response:
             source = response.read()
@@ -85,29 +88,29 @@ class GetArticles:
         articles.set_index('date', inplace=True)
 
         # make dataframe accessible within class
-        searchword.articles = articles
+        query.articles = articles
 
         # location and shape
-        print('Articles with searchword {}'.format(searchword.search))
-        print('Shape of dataframe: ', searchword.articles.shape)
-        print('Shape of dataframe: ', searchword.articles.info())
+        print('Articles with query {}'.format(query.category))
+        print('Shape of dataframe: ', query.articles.shape)
+        print('Shape of dataframe: ', query.articles.info())
 
     # -------------------------------------------------------------------------
 
     # GET MOST COMMON WORDS BY SEARCHWORD--------------------------------------
 
-    def most_common_words(searchword, word=None):
+    def most_common_words(query, word=None):
 
         # create counter with collections Counter()
         results = Counter()
 
         # split strings to words, lowercase and move to results set/counter
-        searchword.articles.headline.astype(str).str.lower().str.split().apply(results.update)
-        searchword.articles.lead.astype(str).str.lower().str.split().apply(results.update)
+        query.articles.headline.astype(str).str.lower().str.split().apply(results.update)
+        query.articles.lead.astype(str).str.lower().str.split().apply(results.update)
 
         # print out words most common
-        print('Most common words in {}'.format(searchword.search))
-        print('# of articles: ', len(searchword.articles.headline))
+        print('Most common words in {}'.format(query.category))
+        print('# of articles: ', len(query.articles.headline))
 
         if word is not None:
             print('Your exact word \'{}\' got mentioned'.format(word), results.get(word), 'times')
@@ -122,11 +125,11 @@ class GetArticles:
 
     # WORD POPULARITY OVER TIME-------------------------------------------------
 
-    def word_over_time(searchword, *words, c='b'):
+    def word_over_time(query, *words, c='b'):
 
         # make everything lowercase and split strings into wordlists
-        searchword.articles.headline = searchword.articles.headline.astype(str).str.lower()
-        searchword.articles.lead = searchword.articles.lead.astype(str).str.lower()
+        query.articles.headline = query.articles.headline.astype(str).str.lower()
+        query.articles.lead = query.articles.lead.astype(str).str.lower()
 
         # lists for needed objects
         dates = []
@@ -137,7 +140,7 @@ class GetArticles:
 
         # loop through wordlists and count occurrence
         for word in words:
-            for date, counth, countl in zip(searchword.articles.index, searchword.articles.headline, searchword.articles.lead):
+            for date, counth, countl in zip(query.articles.index, query.articles.headline, query.articles.lead):
                 count1 = len(re.findall(r'\b{}\w*'.format(word), counth))
                 count2 = len(re.findall(r'\b{}\w*'.format(word), countl))
                 dates.append(date)
@@ -175,15 +178,13 @@ class GetArticles:
 # ------------------------------------------------------------------------------
 
 # SAVE TO EXCEL---------------------------------------------------------------
-    def save_excel(searchword, namefile, loc):
-        searchword.articles.to_excel('{}{}.xlsx'.format(loc, namefile),
-                                     sheet_name=searchword.search)
+    def save_excel(query, namefile, loc):
+        query.articles.to_excel('{}{}.xlsx'.format(loc, namefile),
+                                sheet_name=query.search)
         print('{}.xlsx saved in {}'.format(namefile, loc))
 
 # -----------------------------------------------------------------------------
 
-
-sport_artiklar = GetArticles('sport')
-
-sport_artiklar.most_common_words('fotboll')
-sport_artiklar.word_over_time('fotboll', 'hockey', 'innebandy')
+# sport_artiklar = GetArticles('sport')
+# sport_artiklar.most_common_words('fotboll')
+# sport_artiklar.word_over_time('fotboll', 'hockey', 'innebandy')
